507e55a7738aa637994fc619ddc6bc40bbe9914d
pgap22 <78711175+pgap22@users.noreply.github.com>
2023-02-06 19:10:19 -0600

    Fixed theme and add expired status

--- src/helper/theme.js
+++ src/helper/theme.js
@@ -18,8 +18,10 @@ const theme = (theme) => {
         window.matchMedia("(prefers-color-scheme: dark)").matches)
     ) {
       document.documentElement.classList.add("dark");
+      localStorage.setItem("theme", "dark");
     } else {
       document.documentElement.classList.remove("dark");
+      localStorage.setItem("theme", "light");
     }
   }
 };
